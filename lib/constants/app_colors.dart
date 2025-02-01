fb2cfd60ea7ebf39922ced86775cd2592ad12f9e
alireza <alirezaariye@gmail.com>
2025-02-01 22:52:52 +0330

    feat: Add initial screens and color constants for Filmax app

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+@immutable
+class AppColors{
+  static const orange = Color(0xFFEB8C57);
+  static const darkOraange = Color(0xFFDD6C2C);
+  static const success = Color(0xFF6FCF97);
+  static const error = Color(0xFFEB5757);
+
+}
---
+++ lib/screens/intro_screen.dart
@@ -0,0 +1,62 @@
+import 'package:filmax_app/constants/app_colors.dart';
+import 'package:filmax_app/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+class IntroScreen extends StatelessWidget {
+  const IntroScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Filmax',
+                style: TextStyle(
+                  color: AppColors.darkOraange,
+                  fontSize: 46,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              SizedBox(
+                width: 300,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: AppColors.orange,
+                      foregroundColor: Colors.white),
+                  child: const Text('Login'),
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              SizedBox(
+                width: 300,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const HomeScreen()),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: AppColors.darkOraange,
+                      foregroundColor: Colors.white),
+                  child: const Text('Enter'),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
